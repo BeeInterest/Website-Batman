d2a9029cb46ba71ff619ae3733b2fd59ef34773f
BeeInterest <kolosovasofya0@gmail.com>
2022-02-14 23:55:55 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 const swiper = new Swiper('.main-slider', {
-    slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
     navigation: {
       nextEl: '.main-slider_arrow',
     }, 
+    breakpoints: {
+        320:{
+          slidesPerView: 1,
+        },
+        768:{
+          slidesPerView:2,
+        }
+    }
   });
+  let burger = document.querySelector('.menu-burger');
+  let closeWindow = document.querySelector('.header_menu-close');
+  let menuMobile = document.querySelector('.header');
+
+  burger.addEventListener('click', (e) => {
+      menuMobile.style.display = 'block';
+  })
+  closeWindow.addEventListener('click', (e) => {
+    menuMobile.style.display = 'none';
+})
+
+const modalWindow = document.querySelector('.modal');
+const buttonModal = document.querySelector('.main_button-play');
+
+buttonModal.addEventListener('click', (e) => {
+        modalWindow.classList.add('active');
+})
+
+modalWindow.addEventListener('click', (e) => {
+  const isModal = e.target.closest('.modal_inner');
+  if(!isModal){
+    modalWindow.classList.remove('active');
+  }
+})
